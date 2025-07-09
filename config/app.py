c75a2a1b87247fe2915dd3ed42516a463f1a892a
gfdarj <gfdarj@gmail.com>
2025-07-09 17:16:55 -0300

    lendo as ementas da web

---
+++ config/app.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+class Parametros:
+    def __init__(self):
+        with open(os.getcwd() + '/configuracao.json', encoding='utf-8') as configuracao_json:
+            arquivo_conf = json.load(configuracao_json)
+
+        self._date_format_en = arquivo_conf['date_format_en']
+        self._date_format_br = arquivo_conf['date_format_br']
+        self._url_site = arquivo_conf['url_site']
+        self._url_projetos = arquivo_conf['url_projetos']
+        self._banco_dados = arquivo_conf['banco_dados']
+
+
+    @property
+    def date_format_en(self):
+        return str(self._date_format_en)
+
+    @property
+    def date_format_br(self):
+        return str(self._date_format_br)
+
+    @property
+    def url_site(self):
+        return str(self._url_site)
+
+    @property
+    def url_projetos(self):
+        return self._url_projetos
+
+    @property
+    def banco_dados(self):
+        return str(self._banco_dados)
--- le_ementas.py
+++ le_ementas.py
@@ -1,4 +1,31 @@
 from bs4 import BeautifulSoup
 from bs4.dammit import EncodingDetector
+import requests
+from config import app
+
+meu_app = app.Parametros()
+
+_formato_br = meu_app.date_format_br
+_formato_en = meu_app.date_format_en
+
+for projeto_de_lei in meu_app.url_projetos:
+    r = requests.get(f'{meu_app.url_site}{projeto_de_lei}')
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    tr = soup.find_all("tr")
+
+    for linha in tr:
+        print("-- nova linha --")
+
+        print(linha)
+        tds = linha.find_all("td")
 
 
+        conta = 1
+        for td in tds:
+            #soup_td = BeautifulSoup(td, 'html.parser')
+            print(f"------------- COLUNAS {conta}")
+            print(td.get_text())
+            conta = conta + 1
+
+        print(" ============================= \n\n")
