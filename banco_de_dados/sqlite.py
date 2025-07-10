07aa2df4e9c5c6b96eee3b4e9f1c3ff9169dfea6
gfdarj <gfdarj@gmail.com>
2025-07-09 21:52:55 -0300

    falta começar a gravar

---
+++ banco_de_dados/sqlite.py
@@ -0,0 +1,79 @@
+import sqlite3
+import os
+
+
+class projeto_de_lei:
+    def __init__(self):
+        self.numero = ""
+        self.ementa = ""
+        self.autor = ""
+        self.data_publicacao = ""
+
+#    @property
+#    def numero(self):
+#        return self._numero
+
+#    @property
+#    def ementa(self):
+#        return self._ementa
+
+#    @property
+#    def autor(self):
+#        return self._autor
+
+#    @property
+#    def data_publicacao(self):
+#        return self._data_publicacao
+
+
+class projeto_de_lei_bd:
+    def __init__(self):
+        self._con = sqlite3.connect(os.getcwd() + '/projetos_de_lei.db')
+
+
+    def Insere(self, numero, ementa, data_publicacao, autor):
+        try:
+            cursor = self._con.cursor()
+            cursor.execute('INSERT INTO projetos_de_lei (numero, ementa, data_publicacao, autor) VALUES (?,?,?,?)', (numero, ementa, data_publicacao, autor))
+            self._con.commit()
+            print(f'(sql) Registro {numero} inserido')
+
+        except Exception as ex:
+            print('ERRO: ' + str(ex))
+
+
+    def Atualiza(self, numero, ementa, data_publicacao, autor):
+        try:
+            cursor = self._con.cursor()
+            cursor.execute('UPDATE projetos_de_lei SET ementa = ?, data_publicacao = ?, autor = ? WHERE numero = ?', (ementa, data_publicacao, autor, numero))
+            self._con.commit()
+            print(f'(sql) Registro {numero} atualizado')
+
+        except Exception as ex:
+            print(str(ex))
+
+
+    def Seleciona(self, numero):
+        cursor = self._con.cursor()
+        cursor.execute('SELECT numero, ementa, data_publicacao, autor FROM projetos_de_lei WHERE numero = ?', (numero,)) # QUANDO É 1 PARAMETRO TEM QUE COLOCAR ESSA MALDITA VIRGULA !
+        return cursor.fetchone()
+
+
+    def Apaga(self, numero):
+        cursor = self._con.cursor()
+        cursor.execute('DELETE FROM projetos_de_lei WHERE numero = ?', (numero,)) # QUANDO É 1 PARAMETRO TEM QUE COLOCAR ESSA MALDITA VIRGULA !
+        print(f'(sql) Registro {numero} excluído')
+        return cursor.fetchone()
+
+
+    def ApagaTudo(self):
+        try:
+            cursor = self._con.cursor()
+            cursor.execute('DELETE FROM projetos_de_lei')
+            self._con.commit()
+            print('(sql) Todos os registros apagados')
+
+        except Exception as ex:
+            print('ERRO: ' + str(ex))
+
+
--- le_ementas.py
+++ le_ementas.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 from bs4.dammit import EncodingDetector
 import requests
 from config import app
+from banco_de_dados import sqlite
 
 meu_app = app.Parametros()
 
-_formato_br = meu_app.date_format_br
-_formato_en = meu_app.date_format_en
+pls = []
 
 for projeto_de_lei in meu_app.url_projetos:
     r = requests.get(f'{meu_app.url_site}{projeto_de_lei}')
@@ -15,17 +15,28 @@ for projeto_de_lei in meu_app.url_projetos:
     tr = soup.find_all("tr")
 
     for linha in tr:
-        print("-- nova linha --")
-
-        print(linha)
         tds = linha.find_all("td")
 
+        pl = sqlite.projeto_de_lei()
+        col = 1
 
-        conta = 1
         for td in tds:
-            #soup_td = BeautifulSoup(td, 'html.parser')
-            print(f"------------- COLUNAS {conta}")
-            print(td.get_text())
-            conta = conta + 1
+            if col == 1:
+                pl.numero = td.get_text()
+            # coluna 2 - vazio
+            if col == 3:
+                pl.ementa = td.get_text()
+            if col == 4:
+                pl.data_publicacao = td.get_text()
+            if col == 5:
+                pl.autor = td.get_text()
 
-        print(" ============================= \n\n")
+            col = col + 1
+
+        #print(" ============================= \n\n")
+        pls.append(pl)
+
+print(" *********************************************************************** ")
+print(" *********************************************************************** ")
+for a in pls:
+    print(f"{a.numero}\n{a.ementa}\n{a.autor}\n{a.data_publicacao}\n")
