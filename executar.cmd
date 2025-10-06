372c3ac41ff0e3901baff000b644b18137ff1acb
gfdarj <gfdarj@gmail.com>
2025-10-06 13:42:32 -0300

    Alterado o arquivo batch para salvar cada execução em separado.

--- executar.cmd
+++ executar.cmd
@@ -2,7 +2,28 @@
 
 j:
 cd \Deploy\procura_ementa
+
 rem ***source vprod/bin/activate
 rem .\vprod\Scripts\activate
 
-.\vprod\scripts\python .\le_ementas.py >> output.txt
+:: --- Gera timestamp no formato AAAA-MM-DD_HH-MM-SS ---
+for /f "tokens=1-3 delims=/" %%a in ("%date%") do (
+    set "ano=%%c"
+    set "mes=%%b"
+    set "dia=%%a"
+)
+for /f "tokens=1-3 delims=:." %%a in ("%time%") do (
+    set "hora=%%a"
+    set "min=%%b"
+    set "seg=%%c"
+)
+:: Remove espaços e ajusta zero na hora, se necessário
+set "hora=%hora: =0%"
+
+set "timestamp=%ano%-%mes%-%dia%_%hora%-%min%-%seg%"
+set "output=log_%timestamp%.txt"
+
+echo Executando script Python... > "%output%"
+.\vprod\scripts\python .\le_ementas.py >> "%output%"
+
+echo Log salvo como: %output%
